ca023db4309dfcdb54c45824cfa072f8838c7869
Darius Ray <Drayz@users.noreply.github.com>
2018-11-26 13:21:22 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,61 @@
+import React from "react";
+import BookCase from "./components/Bookcase";
+import * as BooksAPI from "./BooksAPI";
+import Search from "./components/Search";
+import { Route } from "react-router-dom";
+import "./App.css";
+
+class BooksApp extends React.Component {
+  state = {
+    books: []
+  };
+
+  componentDidMount = () => {
+    BooksAPI.getAll()
+      .then(res => {this.setState({ books: res },
+          () => console.log(this.state)
+        );
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  placeBook = (book, shelf) => {
+    console.log(book, shelf);
+    BooksAPI.update(book, shelf)
+    .then( resp => {
+      book.shelf = shelf;
+      this.setState(state => ({books: state.books.filter(b => b.id !== book.id).concat(book)
+      }));
+    });
+  }
+    // <-- want to make use of BooksAPI `update` function
+    // and pass as props from App down to Book, ultimately
+    // flesh out rest of this method to ensure the main page updates after a book is placed -- note that this
+    // will also need to happen when a SearchPage book is placed using this method, too (Don't Repeat Yourself -- use this method on SearchPage, passed
+    // as a prop below)
+
+
+  // search on searchPage
+
+  render() {
+    return (
+      <div>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <BookCase placeBook={this.placeBook} books={this.state.books} />
+          )}
+        />
+        <Route
+          path="/search"
+          render={() => <Search books={this.state.books} handleQuery={this.handleQuery} />}
+        />
+      </div>
+    );
+  }
+}
+
+export default BooksApp;
---
+++ Search.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Book from './Book';
+import * as BooksAPI from "../BooksAPI";
+
+class Search extends Component {
+  // state options: query & searched books that are a response from BooksAPI's search function; remember to chain the promise
+  // `input` should have `onChange` handler, which cues a method on Search to store that value in state and use it to conduct the search.
+
+  state = {
+    query: "",
+    searchBooks: []
+  };
+
+  //Function handles the state of the query
+  //after a input value is placed
+  handleQuery = e => {
+    const value = e.target.value;
+    console.log(value);
+    this.setState({query: value});
+    this.searchBooks(value);
+    // setState of query === value
+    // invoke searchBooks(value)
+  };
+
+  placeBook = (book, shelf) => {
+    console.log(book, shelf);
+    BooksAPI.update(book, shelf)
+    .then( resp => {
+      book.shelf = shelf;
+      this.setState(state => ({books: state.books.filter(b => b.id !== book.id).concat(book)
+      }));
+    });
+  }
+
+  searchBooks = value => {
+    BooksAPI.search(value).then(res => {
+      this.props.books.forEach(book => {
+        res.forEach(newBook => {
+          console.log(newBook);
+          if (book.id === newBook.id) {
+            newBook.shelf = book.shelf;
+          } else {
+            newBook.shelf = 'none'
+          }
+          return newBook;
+        });
+      });
+      this.setState(
+        {searchBooks: res}
+      );
+    });
+    // BooksAPI.search(value) function // you also need to ensure the books displayed on Search show the correct `<select>` shelf option if the book is also on the Main Page.
+  };
+
+  render() {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link to="/" className="close-search" />
+          <div className="search-books-input-wrapper">
+            {/*
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+            <input
+              value={this.state.value}
+              onChange={this.handleQuery}
+              type="text"
+              placeholder="Search by title or author"
+            />
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid" />
+          {this.state.searchBooks.map(book => {
+            return (
+              <Book book={book} key={book.id} handleQuery={this.props.handleQuery}
+              />
+            );
+          })}
+          {this.state.searchBooks.length === 0 && (
+            <h1 style={{textAlign:"center"}}>No results were found</h1>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Search;
